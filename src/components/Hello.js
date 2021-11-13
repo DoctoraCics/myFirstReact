c417b87dadc891dfe5aa7233053104d38c951bf5
Feivel Doctora <feivellixen.doctora.iics@ust.edu.ph>
2021-11-14 04:04:16 +0800

    -Changes

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import helloService from "../services/helloService";
 
 const Hello = () => {
     //function hooks
---
+++ src/services/helloService.js
@@ -0,0 +1,6 @@
+import httpCommon from "../commons/http-common";
+
+const getHello = () => {
+    return httpCommon.get('/hello');
+}
+export default (getHello);
